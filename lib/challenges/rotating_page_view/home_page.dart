6b5c94ed5a53304d5840adab587427ff0bb123b5
msmurali <muralikrishnan0511@gmail.com>
2022-03-07 15:08:12 +0530

    update rotating_page_view/home_page

--- lib/challenges/rotating_page_view/home_page.dart
+++ lib/challenges/rotating_page_view/home_page.dart
@@ -10,15 +10,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final PageController _pageController = PageController(
-    initialPage: 0,
-  );
-  double? _currentPosition;
+  List<Widget> _pages = [];
+  final PageController _controller = PageController(initialPage: 0);
+  double? _currentPage = 0;
 
   @override
   void initState() {
-    _pageController.addListener(() {
-      setState(() => _currentPosition = _pageController.page);
+    _pages = [
+      Container(
+        color: Colors.green,
+        child: const Center(
+          child: Icon(
+            Icons.account_circle,
+            size: 128.0,
+          ),
+        ),
+      ),
+      Container(
+        color: Colors.blue,
+        child: const Center(
+          child: Icon(
+            Icons.security,
+            size: 128.0,
+          ),
+        ),
+      ),
+      Container(
+        color: Colors.brown,
+        child: const Center(
+          child: Icon(
+            Icons.graphic_eq_sharp,
+            size: 128.0,
+          ),
+        ),
+      )
+    ];
+
+    _controller.addListener(() {
+      setState(() {
+        _currentPage = _controller.page;
+      });
     });
     super.initState();
   }
@@ -39,7 +70,13 @@ class _HomePageState extends State<HomePage> {
                 alignment: Alignment.bottomCenter,
                 child: SizedBox.square(
                   dimension: MediaQuery.of(context).size.width,
-                  child: PageViewPage(),
+                  child: PageView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: _pageBuilder,
+                    scrollDirection: Axis.vertical,
+                    controller: _controller,
+                    itemCount: _pages.length,
+                  ),
                 ),
               );
             },
@@ -48,101 +85,41 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
 
-class PageViewPage extends StatefulWidget {
-  @override
-  _PageViewPageState createState() => _PageViewPageState();
-}
-
-class _PageViewPageState extends State<PageViewPage> {
-  List<Widget> _pages = [];
-  final PageController _controller = PageController(initialPage: 0);
-  double? _currentPage = 0;
-
-  @override
-  void initState() {
-    _pages = [
-      Container(
-        color: Colors.green,
-        child: Center(
-          child: Icon(
-            Icons.account_circle,
-            size: 128.0,
+  Widget _pageBuilder(context, index) {
+    if (_currentPage!.floor() + 1 == index) {
+      return Transform.rotate(
+        child: Container(
+          child: _pages[index],
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.red, width: 2.0),
           ),
         ),
-      ),
-      Container(
-        color: Colors.blue,
-        child: Center(
-          child: Icon(
-            Icons.security,
-            size: 128.0,
+        angle: pi * (_currentPage! - index),
+        alignment: Alignment.topRight,
+      );
+    } else if (_currentPage!.floor() == index) {
+      return Transform.rotate(
+        child: Container(
+          child: _pages[index],
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.green, width: 2.0),
           ),
         ),
-      ),
-      Container(
-        color: Colors.brown,
-        child: Center(
-          child: Icon(
-            Icons.graphic_eq_sharp,
-            size: 128.0,
+        angle: pi * (_currentPage! - index),
+        alignment: Alignment.bottomRight,
+      );
+    } else {
+      return Transform.rotate(
+        angle: pi * (_currentPage! - index),
+        alignment: Alignment.topRight,
+        child: Container(
+          child: _pages[index],
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.red, width: 2.0),
           ),
         ),
-      )
-    ];
-
-    _controller.addListener(() {
-      setState(() {
-        _currentPage = _controller.page;
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return PageView.builder(
-      physics: const BouncingScrollPhysics(),
-      itemBuilder: (context, index) {
-        if (_currentPage!.floor() + 1 == index) {
-          return Transform.rotate(
-            child: Container(
-              child: _pages[index],
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.red, width: 2.0),
-              ),
-            ),
-            angle: pi * (_currentPage! - index),
-            alignment: Alignment.topRight,
-          );
-        } else if (_currentPage!.floor() - 1 == index - 1) {
-          return Transform.rotate(
-            child: Container(
-              child: _pages[index],
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green, width: 2.0),
-              ),
-            ),
-            angle: pi * (_currentPage! - index),
-            alignment: Alignment.bottomRight,
-          );
-        } else {
-          return Transform.rotate(
-            angle: pi * (_currentPage! - index),
-            alignment: Alignment.topRight,
-            child: Container(
-              child: _pages[index],
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.red, width: 2.0),
-              ),
-            ),
-          );
-        }
-      },
-      scrollDirection: Axis.vertical,
-      controller: _controller,
-      itemCount: _pages.length,
-    );
+      );
+    }
   }
 }
